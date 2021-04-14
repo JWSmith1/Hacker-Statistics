01966896923e057d9e4f543fb8e7dd549548f753
Jacob Smith <74027890+JWSmith1@users.noreply.github.com>
2021-04-14 00:53:55 -0400

    Add files via upload

---
+++ HackerStats.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+print(np.random.randint(1, 7))
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1, 7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <= 5 :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice)
+print(step)
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
+
+# Plot random_walk
+plt.plot(random_walk)
+
+# Show the plot
+plt.show()
+
+# Simulate random walk 250 times
+all_walks = []
+for i in range(250) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+
+        # Implement clumsiness
+        if np.random.rand() <= 0.001 :
+            step = 0
+
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+plt.plot(np_aw_t)
+plt.show()
+
+# Select last row from np_aw_t: ends
+ends = np_aw_t[-1, :]
+
+# Plot histogram of ends, display plot
+plt.hist(ends)
+plt.show()
+
+
+
